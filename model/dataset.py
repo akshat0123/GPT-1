5c24bd731ebf3bb4f997e263d4400eddec21a493
Your Name <akshat0123@gmail.com>
2021-12-31 14:13:14 -0500

    Added decoding on tokenizer for one-hot vectors

--- model/dataset.py
+++ model/dataset.py
@@ -10,22 +10,9 @@ from tqdm import trange, tqdm
 import torch
 
 
-def get_vmap_from_countfile(path: str, limit: int, unknown: str, 
-                            pad: str) -> Dict[str, int]:
-    """ Takes in path to tsv file mapping words to their frequencies and returns
-        a dictionary mapping vocabulary terms to indices
+def freqs_to_maps(path, limit, unk, pad):
 
-    Args:
-        path: path to counts tsv file
-        limit: maximum vocabulary size
-        unknown: token to represent unknown words
-        pad: token to represent padding
-
-    Returns:
-        (Dict[str, int]): dictionary mapping terms to indices
-    """
-
-    vmap, count = {}, 0
+    vmap, imap, count = {}, {}, 0
     with open(path, 'r') as infile:
 
         line = infile.readline()
@@ -37,24 +24,17 @@ def get_vmap_from_countfile(path: str, limit: int, unknown: str,
 
             line = infile.readline()
             vmap[word] = count
+            imap[count] = word
             count += 1
 
-    vmap[unknown] = count
+    vmap[unk] = count
+    imap[count] = unk
     vmap[pad] = count+1
-    return vmap
+    imap[count+1] = pad
+    return vmap, imap
 
 
-def sopen(filepath: str, linecount: int) -> List[str]:
-    """ Takes in file and returns line by line, providing a progress bar as
-        lines are loaded
-
-    Args:
-        filepath: path of file to read
-        linecount: length of file
-        
-    Returns:
-        (List[str]): list of lines
-    """
+def sopen(filepath, linecount):
 
     lines = []
     progress = tqdm(total=linecount, desc='Loading data')
@@ -67,37 +47,73 @@ def sopen(filepath: str, linecount: int) -> List[str]:
             progress.update(1)
 
     return lines
-        
+
+
+class BooksCorpusTokenizer:
+
+
+    def __init__(self, countpath, start, window, unk, pad, end, vocab):
+        self.vmap, self.imap = freqs_to_maps(countpath, vocab, unk, pad)
+        self.tokenizer = Tokenizer(English().vocab)
+        self.window = window
+        self.start = start
+        self.vocab = vocab
+        self.unk = unk
+        self.pad = pad 
+        self.end = end
+
+
+    def tokenize_line(self, line):
+
+        tokens = [
+            t.text if t.text in self.vmap else self.unk 
+            for t in self.tokenizer(line)
+        ]
+
+        if len(tokens) < self.window + 1:
+            padding = [self.pad for i in range(self.window+1-len(tokens))]
+            tokens = padding + tokens
+
+        ids = torch.Tensor([self.vmap[t] for t in tokens])
+        ids = ids.type(torch.LongTensor)
+        ids = torch.nn.functional.one_hot(ids, self.vocab)
+        xids, yid = ids[:-1], ids[-1]
+
+        return xids, yid
+
+
+    def tokenize(self, lines):
+
+        X = torch.empty((len(lines), self.window, self.vocab))
+        Y = torch.empty((len(lines), self.vocab))
+
+        for i in range(len(lines)):
+            x, y = self.tokenize_line(lines[i])
+            X[i], Y[i] = x, y
+
+        return X, Y
+
+    
+    def decode_line(self, x):
+        ids = torch.argmax(x, dim=1)
+        tokens = [self.imap[i.item()] for i in ids]
+        return tokens
+
+
+    def decode(self, x):
+        lines = []
+
+        for i in range(x.shape[0]):
+            lines.append(self.decode_line(x[i]))
+
+        return lines
+
 
 class BooksCorpus(Dataset):
 
 
-    def __init__(self, datapath: str, countpath: str, window: int, 
-                 vocab: int=float('inf'), unknown: str='<UNK>', 
-                 pad: str='<PAD>', linecount: int=None) -> 'BooksCorpus':
-        """ Dataset class for BooksCorpus dataset
-
-        Args:
-            datapath: path containing split BooksCorpus texts
-            countpath: path listing term frequencies
-            window: size of window for each line of text
-            vocab: vocabulary size
-            unknown: token to use for unknown tokens
-            pad: token to use for padding
-            linecount: number of lines in dataset (optional parameter that
-                       provides a loading bar if provided)
-
-        Returns:
-            (BooksCorpus): BooksCorpus dataset instance
-        """
-
+    def __init__(self, datapath, linecount):
         super(Dataset, self).__init__()
-
-        self.vmap = get_vmap_from_countfile(countpath, vocab, unknown, pad)
-        self.tokenizer = Tokenizer(English().vocab)
-        self.unknown = unknown
-        self.window = window
-        self.pad = pad
 
         if linecount is None:
             self.data = open(datapath, 'r').readlines()
@@ -107,36 +123,12 @@ class BooksCorpus(Dataset):
 
 
     def __len__(self) -> int:
-        """ Returns dataset size
 
-        Returns:
-            (int): dataset size
-        """
         return len(self.data)
 
 
-    def __getitem__(self, idx: int) -> (torch.Tensor, torch.Tensor):
-        """ Returns item at specified index
+    def __getitem__(self, idx):
 
-        Args:
-            idx: index of item to return
+        return self.data[idx].strip()
 
-        Returns:
-            (torch.Tensor): list of token ids of size {self.window}
-            (torch.Tensor): token id of next word (to be predicted)
-        """
 
-        tokens = [
-            t.text if t.text in self.vmap else self.unknown 
-            for t in self.tokenizer(self.data[idx].strip())
-        ]
-
-        if len(tokens) < self.window + 1:
-            padding = [self.pad for i in range(self.window+1-len(tokens))]
-            tokens = padding + tokens
-
-        ids = torch.Tensor([self.vmap[t] for t in tokens])
-        ids = ids.type(torch.LongTensor)
-        xids, yid = ids[:-1], ids[-1]
-
-        return xids, yid
